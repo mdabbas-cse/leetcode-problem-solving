ce35f9e7d334c11b7475fef4e69e17997f960fed
mdabbas-cse <gmabbas44@gmail.com>
2022-07-06 16:18:35 +0600

    valid anagram

--- solved with js/242. Valid Anagram.js
+++ solved with js/242. Valid Anagram.js
@@ -5,6 +5,7 @@ const s = "rat"
 const t = "car"
 
 var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false
   const s_spl = s.split('').sort().join('')
   const t_spl = t.split('').sort().join('')
   return s_spl === t_spl
